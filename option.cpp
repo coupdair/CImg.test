de2c17a6ab8a761fb28e1218a1e06415a1338dc5
Sebastien COUDERT <sebastien.coudert@univ-lille1.fr>
2012-04-09 21:57:01 +0200

    option: small changes modified:   option.cpp: change -O3 to -O0 compilation optimisation level, cimg::info() called on boolean value.

--- option.cpp
+++ option.cpp
@@ -1,5 +1,5 @@
 //CImg command line option test
-//compilation: g++ -o option option.cpp -I../CImg -Wall -W -lm -ansi -pedantic -O3 -fno-tree-pre -Dcimg_use_vt100 -Dcimg_display=0 #no GUI
+//compilation: g++ -o option option.cpp -I../CImg -Wall -W -lm -ansi -pedantic -O0 -fno-tree-pre -Dcimg_use_vt100 -Dcimg_display=0 #no GUI
 //               -I/usr/X11R6/include -Dcimg_use_xshm -Dcimg_use_xrandr -L/usr/X11R6/lib -lpthread -lX11 -lXext -lXrandr #GUI
 //run line: ./option -I -h
 //run line: ./option -I -i ../CImg/examples/img/parrot_original.ppm
@@ -18,7 +18,7 @@ using namespace cimg_library;
 int main(int argc,char **argv)
 {
   //Display program usage, when invoked from the command line with option '-h'.
-  cimg_usage("CImg command line option test program\nusage:\t./option -h -I\n\t./option -I -i ../CImg/examples/img/parrot_original.ppm");
+  cimg_usage("CImg command line option test program\n usage:\t./option -h -I\n\t./option -I -i ../CImg/examples/img/parrot_original.ppm");
   //get if help is requested
   const bool help=cimg_option("-h",false,"showing help only");
   //get a file name from command line (or use default)
@@ -26,7 +26,7 @@ int main(int argc,char **argv)
   //get a floating point value from command line (or use default)
   const double value=cimg_option("--value",1.2345,"optional value");
   //show compilation options, such as debug, GUI, external library (e.g. FTTw), ...
-  if( cimg_option("-I",(const char*)NULL,"show compilation options") != NULL ) cimg_library::cimg::info();
+  if(cimg_option("-I",false,"show compilation options")) cimg_library::cimg::info();
   //exit if help requested
   if(help) return 0;
   //show value
